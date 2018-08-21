e386968655c418a4d20653aebb85156f475ffc9e
Mor3ta <moreta.concepcion@gmail.com>
2018-08-21 11:50:32 -0400

    delete method play and pedir from Player class, add method revelar and pedir to Crupier

--- Blackjack.py
+++ Blackjack.py
@@ -1,5 +1,26 @@
 import random
 
+class Crupier():
+    
+    def __init__(self):
+        self.mano=[]
+
+    def repartir (self) :
+        mazo=['As', '2', '3', '4', '5','6','7','8','9','10','j','Q','k']
+        Player.mano=random.sample(mazo,  2)
+        self.mano= random.sample(mazo,  1)
+    def revelar (self):
+        carta=random.randint(1, 13)
+        self.mano.append(carta)
+
+    def pedir(self):
+        if self.puntos<21:
+            pedir=random.randint(1, 13)
+            self.mano.append(pedir)
+            self.puntos=self.puntos+pedir
+            print self.mano
+            print self.puntos    
+
 class Player():
     puntos=0
     def __init__(self,nombre,dinero,apuesta):
@@ -10,7 +31,7 @@ class Player():
         self.puntos=self.puntos
         
     def play(self):
-        pick=random.sample([1, 2, 3, 4, 5,6,7,8,9,10,11,12,13],  2)
+        pick=random.sample(['As', '2', '3', '4', '5','6','7','8','9','10','j','Q','k'],  2)
         self.mano=pick
         puntos=pick[0]+pick[1]
         self.puntos=puntos
@@ -32,23 +53,21 @@ class Player():
         self.pedir()
 
 
-class Crupier():
-    def __init__(self):
-        self.mano=[]
 
-    def repartir (self) :
-        Player.mano=random.sample([1, 2, 3, 4, 5,6,7,8,9,10,11,12,13],  2)
-        self.mano=random.sample([1, 2, 3, 4, 5,6,7,8,9,10,11,12,13],  2)
         
 
 
 x= Crupier()
 x.repartir()
-
-print Player.mano
+print x.mano
+x.revelar()
 print x.mano
 
+
+
+
         
+
 
    
 
