d3a1f4ebc085521a6d7a051341ffff26ba934a41
Mor3ta <moreta.concepcion@gmail.com>
2018-09-06 12:32:20 -0400

    agregue juegada del crupier, comentarios y cambios en algunas varialbles

--- Blackjack.py
+++ Blackjack.py
@@ -1,41 +1,44 @@
 import random
 
 class Game():
+    mazo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    puntos_Crupier=0
+    catas_crupier=0
 
     def __init__(self,nombre,dinero):
         self.nombre= nombre
         self.mano=[]
-        self.dinero=dinero  # cantidad de dinero al iniciar el juego.
+        self.dinero=dinero
         self.apuesta=0
         self.puntos=0
-        self.puntos_crupier=0
-        self.cartas_crupier=[]
+
 
         
- # Accion del jugador, Recibe el monto que el jugador decea apostar
- # y devuelve dos cartas y la cantidad de punto de este.
+           # Accion del jugador, Recibe el monto que el jugador decea apostar
+           # y devuelve dos cartas y la cantidad de punto de este. Y la carta inicial de Crupier.
  
     def apostar(self,apostar):
-        mazo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
         self.apuesta=apostar
-        self.mano= random.sample(mazo,  2)
-        self.cartas_crupier=random.sample(mazo,  1)
-        print (self.mano)
-        for i in self.mano:
-            self.puntos=self.puntos +i
-        print (self.puntos)
-        print (self.cartas_crupier)
+        if self.apuesta > self.dinero:
+            print("El dinero no es suficiente para esta apuesta.")    # Muestra si el dinero que tienes es suficiente
+        else:                                                         #Para la apuesta que quieres realizar.
+            self.mano= random.sample(self.mazo,  2)
+            self.cartas_crupier=random.sample(self.mazo,  1)      #Agrega de manera random una carta al Crupier y Dos al jugador.
+            self.puntos = sum(self.mano)
+            print ("Cartas de {} {}".format(self.nombre,self.mano))
+            print ("Cartas del crupier{}".format(self.cartas_crupier))
+            print("Puntos de {} {}".format(self.nombre, self.puntos))
 
 
     # Agrega una carta al jugador, suma los puntos.
            
     def pedir(self):
         if self.puntos<21:
-            pedir=random.randint(1, 13)
+            pedir=random.randint(1,13)
             self.mano.append(pedir)
             self.puntos=self.puntos+pedir
-            print (self.mano)
-            print (self.puntos)
+            print("{} pidio una carta {}".format(self.nombre,self.mano))
+            print("Nueva puntuacion de {}:{}".format(self.nombre, self.puntos))
 
 
     # Dobla la apuesta iniciar y agregar una carta a la mano.
@@ -44,20 +47,31 @@ class Game():
     def duplicar(self):
         self.apuesta= self.apuesta *2
         self.dinero= self.dinero-self.apuesta
-        self.pedir() 
+        self.pedir()
+        self.terminar()
+
+    #Finaliza el turno del jugador e inicia el turno del Crupier.
+    #Devuelve quien es el ganador del juego.
 
     def terminar(self):
-        pedir = random.randint(1, 13)
-        self.cartas_crupier.append(pedir)
-        for i in self.cartas_crupier:
-            self.puntos_crupier = self.puntos_crupier + i
-        print(self.puntos_crupier)
-        print (self.cartas_crupier)
-        if self.puntos > self.puntos_crupier:
-            print ("Player win")
-        else:
-            self.dinero = self.dinero - self.apuesta
-            print ("Player Lose ")
+        print("{} termino la partida, el Crupier inicia a jugar." .format(self.nombre))
+        revelar = random.randint(1, 13)
+        self.cartas_crupier.append(revelar)       #Muestra la carta oculta del crupier
+        self.puntos_crupier = sum(self.cartas_crupier)
+        while self.puntos_crupier < 17:
+            self.cartas_crupier.append(revelar)
+            self.puntos_crupier= sum(self.cartas_crupier) #Pide Cartas al Crupier hasta que este llega a una puntuacion de 17
+        print("Cartas del Crupier:{}".format(self.cartas_crupier))
+        print ("Puntos del Curpier:{}".format(self.puntos_crupier))
+
+        #RESULTADO
+        # if self.puntos > self.puntos_crupier:
+        #     self.dinero = self.dinero+self.apuesta *2
+        #     print ("${}: WIN  Tu cantidad de Dinero actual es ${}".format(self.nombre,self.dinero))
+        #
+        # else:
+        #     self.dinero = self.dinero - self.apuesta
+        #     print ("{}: Lose ${} Tu cantidad de Dinero actual es ${}".format(self.nombre,self.apuesta,self.dinero))
 
 
 
@@ -65,15 +79,14 @@ class Game():
 
 
 Lucero=Game("Lucero", 5000)
-Lucero.apostar(500)
+Lucero.apostar(1000)
 Lucero.pedir()
 Lucero.terminar()
 print(Lucero.dinero)
 
 
 
-#NOTAS:
-#AGREGAR  CARTAS A,J,Q,K.
+
 
 
         
